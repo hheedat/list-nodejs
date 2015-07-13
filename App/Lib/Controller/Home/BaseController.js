f24367caace9c5511cf6bf6b279fd651719a21b3
hheedat <yuxiangzhao@outlook.com>
2015-07-13 21:36:07 +0800

    change base controller

--- App/Lib/Controller/Home/BaseController.js
+++ App/Lib/Controller/Home/BaseController.js
@@ -5,37 +5,41 @@
  * @return {[type]}         [description]
  */
 var util = require('util');
-module.exports = Controller(function() {
-	'use strict';
-	return {
-		init: function(http) {
-			this.super("init", http);
-			//其他的通用逻辑
+module.exports = Controller(function () {
+    'use strict';
+    return {
+        init: function (http) {
+            this.super("init", http);
+            //其他的通用逻辑
             var self = this;
             //console.log("http:  " + util.inspect(http));
-			if(http.action === "login" || http.action === "register" ||http.action==="checkMail"){
-                return ;
-            }else{
-                return self.session("userInfo").then(function(data){
-                    if(!isEmpty(data)){
-                        if(http.controller !== "List"){
-                            //return self.redirect("list");
-                        }
-                        self.userInfo = data;
-                        self.assign({
-                            'islogin':true,
-                            'name':self.userInfo.name
-                        });
-                    }else{
-                        if(http.action !== "index"){
-                            self.assign({
-                               "info":"您还没有登录"
-                            });
-                            return self.display("index");
-                        }
+            console.log("action ->",http.action,"  controller ->",http.controller);
+
+            return self.session("userInfo").then(function (data) {
+                if (!isEmpty(data)) {
+                    console.log("分支 1");
+                    if (http.action === "logout") {
+                        return;
                     }
-                });
-            }
-		}
-	}
+                    if (http.controller !== "List") {
+                        return self.redirect("/list");
+                    }
+                    self.userInfo = data;
+                    self.assign({
+                        'islogin': true,
+                        'name': self.userInfo.name
+                    });
+                } else {
+                    console.log("分支 2");
+                    if (http.controller === "Index") {
+                        return;
+                    }
+                    self.assign({
+                        "info": "您还没有登录"
+                    });
+                    return self.redirect("/");
+                }
+            });
+        }
+    }
 })
