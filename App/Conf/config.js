cbea277abd6f0c97f6e10c3f14dda5e7def56ce9
zhaoyuxiang <yuxiangzhao@outlook.com>
2015-08-22 17:56:10 +0800

    change config.js

--- App/Conf/config.js
+++ App/Conf/config.js
@@ -9,4 +9,11 @@ module.exports = {
   db_pwd: '', // 密码
   db_prefix: 'think_', // 数据库表前缀
   tpl_file_suffix: '.ejs', //模版文件名后缀
+
+  session_name: 'yao_yao_qie_ke_nao', //session对应的cookie名称
+  session_type: 'File', //session存储类型, 空为内存，还可以为File
+  session_path: '', //File类型下文件存储位置，默认为系统的tmp目录
+  session_options: {}, //session对应的cookie选项
+  session_sign: '', //session对应的cookie使用签名
+  session_timeout: 24 * 3600 * 14, //服务器上session失效时间，单位：秒
 };
